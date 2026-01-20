1cdc74f0fbf88d2c085b04cd487dd630fc15932e
bayo <bayodeog@gmail.com>
2026-01-20 12:34:36 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Adebayode Adegoke on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
