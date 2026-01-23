2a024f1e488019839b56ad07c2e5ecc5e145d9e9
bayo <bayodeog@gmail.com>
2026-01-23 09:26:36 +0100

    Latest development - one button conditional challenge, const vs var

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,30 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
-           
-            Spacer()
             
-            HStack {
-                
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
-               
-                Button("Great") {
-                    message = "Great!"
+            Spacer()
+        
+            Button("Press Me") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else {
+                    message = message1
+                    imageString = imageString1
                 }
             }
             .buttonStyle(.borderedProminent)
