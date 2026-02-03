8a8d85c56401a61830e5f88f9b37ab1346e8413d
bayo <bayodeog@gmail.com>
2026-02-03 15:27:31 +0100

    Recent updates

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,37 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 24))
+                .shadow(radius: 20)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
-        
-            Button("Press Me") {
+            
+            Button("Show Message") {
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                if message == message1 {
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
+                //                let imageName1 = "image0"
+                //                let imageName2 = "image1"
+                //                if message == message1 {
+                //                    message = message2
+                //                    imageName = imageName2
+                //                } else {
+                //                    message = message1
+                //                    imageName = imageName1
+                //                }
+                message = (message == message1 ? message2 : message1)
+                //                imageName = (imageName == "image0" ? "image1" : "image0")
+                
+                
+                imageName = "image\(imageNumber)" //Other way to write this imageName = "image" + Spring(imageNumber)
+            
+                imageNumber += 1 //Other way to write this imageNumber = imageNumber + 1
+                if imageNumber > 9 {
+                    imageNumber = 0
                 }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(Color.orange)
             
         }
         .padding()
